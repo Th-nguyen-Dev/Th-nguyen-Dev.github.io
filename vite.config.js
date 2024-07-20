24039770102ccdacff530841222d762a8e258d91
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-07-19 19:20:15 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/th-nguyen-dev.github.io/",
+  base: "/th-nguyen-dev.github.io",
   plugins: [react()],
 })
