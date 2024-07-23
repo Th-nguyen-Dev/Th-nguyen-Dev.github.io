13fa3cef10ece1be52eb9eb96d8562c927889698
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-07-23 01:24:17 -0700

    Add Emissive map

--- src/Template_3D.jsx
+++ src/Template_3D.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import earthAlbedo from "/textures/earth albedo dec.png";
-import earthBump from "/textures/earth elevation complete.png";
+import earthBump from "/textures/earth elevation.png";
 import earthSpecular from "/textures/earth land ocean mask.png";
+import earthNight from "/textures/earth night_lights_modified.png";
 import galaxy from "/textures/Galaxy.png";
 
 import cloud from "/textures/earth clouds.jpg";
@@ -26,13 +27,19 @@ const earthGeometry = new THREE.SphereGeometry(10, 400, 400, 0, Math.PI * 2, 0, 
 const earthTexture = new THREE.TextureLoader().load(earthAlbedo);
 const earthBumpTexture = new THREE.TextureLoader().load(earthBump);
 const earthSpecularTexture = new THREE.TextureLoader().load(earthSpecular);
+const earthNightTexture = new THREE.TextureLoader().load(earthNight);
+const earthEnvTexture = new THREE.TextureLoader().load(galaxy);
 const earthMaterial = new THREE.MeshPhongMaterial({ 
     map: earthTexture, 
+
+    emissiveMap: earthNightTexture,
+    emissiveIntensity: 1,
+    emissive: 0xffff00,
+
+    envMap: earthEnvTexture,
     
-
-
     bumpMap: earthBumpTexture, 
-    bumpScale: 50, 
+    bumpScale: 100, 
 
     specularMap: earthSpecularTexture, 
     shininess: 100,
@@ -58,7 +65,7 @@ scene.add(earthCloud);
 
 //Add light
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 2);
-const ambientLight = new THREE.AmbientLight(0xfffffff,0.02);
+const ambientLight = new THREE.AmbientLight(0xfffffff,0.005);
 
 // const lightHelper = new THREE.DirectionalLightHelper(directionalLight);
 // scene.add(lightHelper);
@@ -90,7 +97,7 @@ function animate(){
     earth.rotateOnAxis(axis, angle);
 
     //Rotate Cloud
-    earthCloud.rotateOnAxis(axis, angle*0.6);
+    earthCloud.rotateOnAxis(axis, angle*1.5);
     renderer.render(scene,camera);
 
     control.update();
