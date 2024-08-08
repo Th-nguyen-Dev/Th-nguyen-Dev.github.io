5ca389d283021acdc77ffb33d3aa94a8c149b5e6
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-08-07 19:10:37 -0700

    Cleaning up files

--- src/SharedCameraState.jsx
+++
@@ -1,7 +0,0 @@
-import { useRef } from 'react';   
-import * as THREE from 'three';
-function ShareCameraState() {
-    const cameraRef = useRef(new THREE.PerspectiveCamera());
-    return cameraRef;
-}
-export default ShareCameraState;
