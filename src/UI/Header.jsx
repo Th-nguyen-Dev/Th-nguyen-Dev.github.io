a1a982e56ecd651e287f7a44fedbcaeaeb9c6fcc
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-09-16 02:08:34 -0700

    Fix a bug with the header and replace header hint

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -16,29 +16,42 @@ import redux_store from '@/context/redux_store.jsx';
 
 import '@/index.css';
 import Triangle from '../../public/readme_assets/Triangle.svg';
-function Header(){
-    return(
+
+
+const onSelect = (event) => {
+    event.target.blur(); 
+}
+
+function Header() {
+    return (
         <header className='sticky top-0 hover:translate-y-0 -translate-y-20 transition-all h-36'>
             <div className='absolute w-full h-20 bg-background'></div>
             <Provider store={redux_store}>
-                    <NavigationMenu >
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuLink className={navigationMenuTriggerStyle()} > Github </NavigationMenuLink>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <NavigationMenuLink className={navigationMenuTriggerStyle()}> LinkedIn </NavigationMenuLink>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <NavigationMenuLink className={navigationMenuTriggerStyle()}> Behance </NavigationMenuLink>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()} href="https://github.com/Th-nguyen-Dev" target="_blank" onClick={onSelect}>
+                                Github
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()} href="https://www.linkedin.com/in/hung-nguyen-dev/" target="_blank" onClick={onSelect}>
+                                LinkedIn
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()} href="https://www.behance.net/ryannguyen35" target="_blank" onClick={onSelect}>
+                                Behance
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
             </Provider>
-            <div className=" w-full items-center justify-center flex"> 
-                <img src = {Triangle} className="w-8"></img>
+            <div className="w-full items-center justify-center flex">
+                <img src={Triangle} className="w-8"></img>
             </div>
         </header>
     );
 }
+
 export default Header;
