6e5e2f6c2c3f0981a0eead63555824ce8f80cc5d
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-09-18 12:34:50 -0700

    Further refine aspect ration problem #7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
     darkMode: ["class"],
     content: [
@@ -6,7 +7,15 @@ export default {
 		'./src/**/*.{js,jsx,ts,tsx}'
 	],
   theme: {
+
   	extend: {
+		fontSize:{
+			'xxs': '.4rem'
+		},
+		screens:{
+			'xs': '475px',
+			...defaultTheme.screens
+		},
 		transitionProperty: {
 			'height': 'height',
 			'width': 'width',
