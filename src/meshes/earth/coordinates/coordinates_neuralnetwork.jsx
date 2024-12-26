44cee45ff9e86abe7efee206ad5cd11436951d8f
Ryan Nguyen <th.nguyen.developer@gmail.com>
2024-12-25 17:33:10 -0800

    enable both NeuralNetwok and Timelne Coordinates

--- src/meshes/earth/coordinates/coordinates_neuralnetwork.jsx
+++ src/meshes/earth/coordinates/coordinates_neuralnetwork.jsx
@@ -5,7 +5,7 @@ import CoordinateMeshesGeneration from './coordinates_lib';
 import { regenerateDictionary } from '@/context/reducer/locations/locations_neural_network';
 
 const CoordinatesNeuralNetwork = () => {
-    const coordinates = useSelector((state) => state.locationsNNDictionary.coordinates3D);
+    const coordinates3D = useSelector((state) => state.locationsNNDictionary.coordinates3D);
     const radius = useSelector((state) => state.locationsNNDictionary.radius);
     const color = new THREE.Color(0xffa500);
     const emissiveColor = new THREE.Color(0xffa500);
@@ -14,7 +14,7 @@ const CoordinatesNeuralNetwork = () => {
 
     return (
         <CoordinateMeshesGeneration 
-            coordinates={coordinates}
+            coordinates={coordinates3D}
             color={color}
             emissiveColor={emissiveColor}
             emissiveIntensity={emissiveIntensity}
